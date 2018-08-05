26f9044d29d1f1d9729530721b4be3c7cffb3ec4
kamrul <kamrul_bd_islam@yahoo.com>
2018-08-05 09:43:04 +0600

    Front End Designed and Added
    Front End done with job post and job category

---
+++ application/views/frontend/master.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Job Listing</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>/FrontendAsset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url();?>/FrontendAsset/css/shop-item.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Job Finder</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-3">
+          <h1 class="my-4">Job Category</h1>
+          <div class="list-group">
+            <a href="#" class="list-group-item active">IT/Programming</a>
+            <a href="#" class="list-group-item">Corporate/Business</a>
+            <a href="#" class="list-group-item">Education/Training</a>
+            <a href="#" class="list-group-item">Corporate/Business</a>
+            <a href="#" class="list-group-item">Education/Training</a>
+          </div>
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9">
+
+          <!-- /.card -->
+          <div class="card card-outline-secondary my-4">
+            <div class="card-header">
+              Job Title  | Company Name
+            </div>
+            <div class="card-body">
+              <p><strong>Vacancy : 02</strong></h4>
+              <h4><u>Job Requirements</u></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, </p>
+              <ul>
+                <li>consectetur adipisicing elit. Omnis</li>
+                <li>sfsconsectfg etur adiptrh isicing elit. </li>
+                <li>fg bfgconsectetur adipisicing elit is</li>
+                <li>dgdconsectetur adipisicing </li>
+                <li>dconsectetur adipisicing elit</li>
+                <li>cdbdfonsectetur adipisicing Omnis</li>
+              </ul>             
+              <hr>              
+              <small class="text-muted">Published Date 3/1/17</small> | <small class="text-muted">Last Date 3/1/17</small>
+              <hr>      
+              <a href="#" class="btn btn-success">Apply Online</a>
+            </div>
+          </div>
+          <!-- /.card -->
+
+          <!-- /.card -->
+          <div class="card card-outline-secondary my-4">
+            <div class="card-header">
+              Job Title  | Company Name
+            </div>
+            <div class="card-body">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>             
+              <hr>              
+              <small class="text-muted">Published Date 3/1/17</small> | <small class="text-muted">Last Date 3/1/17</small>
+              <hr>      
+              <a href="#" class="btn btn-success">Apply Online</a>
+            </div>
+          </div>
+          <!-- /.card -->
+          <!-- /.card -->
+          <div class="card card-outline-secondary my-4">
+            <div class="card-header">
+              Job Title  | Company Name
+            </div>
+            <div class="card-body">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>             
+              <hr>              
+              <small class="text-muted">Published Date 3/1/17</small> | <small class="text-muted">Last Date 3/1/17</small>
+              <hr>      
+              <a href="#" class="btn btn-success">Apply Online</a>
+            </div>
+          </div>
+          <!-- /.card -->
+        
+        </div>
+        <!-- /.col-lg-9 -->
+
+      </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Job Listing 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url();?>/FrontendAsset/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>/FrontendAsset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
