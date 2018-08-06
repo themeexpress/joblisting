7c8835a03f495f869f57d5ae343786748e127855
ThemeExpress <kkislam33@gmail.com>
2018-08-06 18:25:31 +0600

    admin dashboard content update

--- application/views/admin/admin_dashboard.php
+++ application/views/admin/admin_dashboard.php
@@ -79,7 +79,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Developed By Md. Kamrul Islam</a>.</strong> All rights
     reserved.
   </footer>
 
--- application/views/admin/includes/admin_dashboard_content.php
+++ application/views/admin/includes/admin_dashboard_content.php
@@ -7,7 +7,7 @@
             <div class="inner">
               <h3>150</h3>
 
-              <p>New Orders</p>
+              <p>Total Applications</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
@@ -20,9 +20,9 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
+              <h3>53</h3>
 
-              <p>Bounce Rate</p>
+              <p>Total Jobs</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
@@ -37,7 +37,7 @@
             <div class="inner">
               <h3>44</h3>
 
-              <p>User Registrations</p>
+              <p>Total Register Users</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
